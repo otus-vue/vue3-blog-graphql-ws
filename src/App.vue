7a6945e4c5080e024c11224be53e9d980f36c586
Ilya Nurullin <nurullinin@mail.ru>
2023-02-21 21:11:15 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <Navigation></Navigation>
+  <router-view name="sidebar"></router-view>
+  <router-view></router-view>
+</template>
+
+<script setup>
+import Navigation from "./views/general/Navigation.vue";
+</script>
---
+++ src/views/general/Navigation.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <button
+        class="navbar-toggler navbar-toggler-right"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarResponsive"
+        aria-controls="navbarResponsive"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+<!--            <a class="nav-link" href="#">Home</a>-->
+            <router-link class="nav-link" :to="{ name: 'home' }">Home</router-link>
+          </li>
+          <li class="nav-item">
+<!--            <a class="nav-link" href="#">About</a>-->
+            <router-link class="nav-link" :to="{ name: 'about' }">About</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ name: 'post', params: { postId: 15 } }">Sample Post</router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  position: fixed!important;;
+  top:0!important;;
+  background: #222!important;
+}
+</style>
+
+<script>
+export default {};
+</script>
