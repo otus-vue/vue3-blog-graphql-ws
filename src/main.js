4ad6dabc0a74e453e8c1f23725a57007d8517cc6
Ilya Nurullin <nurullinin@mail.ru>
2023-02-22 14:51:55 +0500

    code from class

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import { createApp } from 'vue'
+import {createApp, h, provide} from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import ApolloClient from 'apollo-boost'
+import {DefaultApolloClient} from "@vue/apollo-composable";
+
+const apolloClient = new ApolloClient({
+    // You should use an absolute URL here
+    uri: 'http://localhost:4000/graphql'
+})
+
+const app = createApp({
+    setup () {
+        provide(DefaultApolloClient, apolloClient)
+    },
+
+    render: () => h(App),
+})
 
 app.use(router)
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,68 +12,25 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto">
-            <div class="post-preview">
-              <a href="post.html">
-                <h2 class="post-title">
-                  Man must explore, and this is exploration at its greatest
-                </h2>
-                <h3 class="post-subtitle">
-                  Problems look mighty small from 150 miles up
-                </h3>
-              </a>
-              <p class="post-meta">
-                Posted by
-                <a href="#">Start Bootstrap</a>
-                on September 24, 2019
-              </p>
-            </div>
-            <hr />
-            <div class="post-preview">
-              <a href="post.html">
-                <h2 class="post-title">
-                  I believe every human has a finite number of heartbeats. I
-                  don't intend to waste any of mine.
-                </h2>
-              </a>
-              <p class="post-meta">
-                Posted by
-                <a href="#">Start Bootstrap</a>
-                on September 18, 2019
-              </p>
-            </div>
-            <hr />
-            <div class="post-preview">
-              <a href="post.html">
-                <h2 class="post-title">
-                  Science has not yet mastered prophecy
-                </h2>
-                <h3 class="post-subtitle">
-                  We predict too much for the next year and yet far too little
-                  for the next ten.
-                </h3>
-              </a>
-              <p class="post-meta">
-                Posted by
-                <a href="#">Start Bootstrap</a>
-                on August 24, 2019
-              </p>
-            </div>
-            <hr />
-            <div class="post-preview">
-              <a href="post.html">
-                <h2 class="post-title">Failure is not an option</h2>
-                <h3 class="post-subtitle">
-                  Many say exploration is part of our destiny, but it’s actually
-                  our duty to future generations.
-                </h3>
-              </a>
-              <p class="post-meta">
-                Posted by
-                <a href="#">Start Bootstrap</a>
-                on July 8, 2019
-              </p>
-            </div>
-            <hr />
+            <h1 v-if="loading">Loading ...</h1>
+            <template v-else v-for="post in result.posts" :key="post.id">
+              <div class="post-preview">
+                <a href="post.html">
+                  <h2 class="post-title">
+                    {{ post.title }} - #{{ post.id }}
+                  </h2>
+                  <h3 class="post-subtitle">
+                    {{ post.text }}
+                  </h3>
+                </a>
+                <p class="post-meta">
+                  Posted by
+                  <a href="#">{{ post.author?.name ?? 'undefined' }}</a>
+                  on {{ post.createdAt }}
+                </p>
+              </div>
+              <hr />
+            </template>
             <!-- Pager -->
             <div class="clearfix">
               <a class="btn btn-primary float-right" href="#"
@@ -91,13 +48,54 @@
 import PageTemplate from "./general/PageTemplate.vue";
 import PageHeader from "./general/PageHeader.vue";
 import {useRoute, useRouter} from "vue-router";
+import {useQuery} from "@vue/apollo-composable";
+import gql from "graphql-tag";
+import {reactive, ref} from "vue";
 
 const router = useRouter()
 const route = useRoute()
 
-console.log(route);
+// const enabled = ref(true)
+
+const result = reactive({})
+const loading = ref(true)
+
+// const { result, loading } = useQuery(gql`query ($userId:ID!){
+// posts { id title text author { name } createdAt }
+// user(id:$userId) { id name avatar(size: S_512) }
+// }`, { userId: 3 }, { enabled })
+//
+// console.log(result);
+
+const socket = new WebSocket('ws://localhost:4000/ws')
+
+console.log(socket)
+
+socket.onopen = (event) => {
+  console.log('open', event)
+  socket.send(makeEvent('getPosts'))
+}
+socket.onclose = (event) => console.log('close', event)
+socket.onmessage = (event) => {
+  console.log('message', event)
+  const data = JSON.parse(event.data)
+
+  if (data.event === 'updatePosts') {
+    result.posts = data.data.posts
+    loading.value = false
+  }
+  if (data.event === 'addPost') {
+    result.posts.push(data.data.newPost)
+  }
+}
+socket.onerror = (event) => console.log('error', event)
+
 
 function goToAbout() {
   router.push({ name: 'about' })
+}
+
+function makeEvent(eventName, data = {}) {
+  return JSON.stringify({ event: eventName, data })
 }
 </script>
